1dd3a91ca21d88e908093714cc6b20520695788a
Siddu77 <siddanagouda2012@gmail.com>
2024-02-12 16:20:28 +0530

    Revert "merge test 2"
    This reverts commit aab137c7dcbcd91ac1b3ed7a2a358d13b29d3ed5.

--- src/main/java/com/sjt/userService/controller/UserController.java
+++ src/main/java/com/sjt/userService/controller/UserController.java
@@ -2,7 +2,9 @@ package com.sjt.userService.controller;
 
 import com.sjt.userService.entity.User;
 import com.sjt.userService.service.UserService;
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +20,6 @@ public class UserController {
 
     @GetMapping("/user/{id}")
     public ResponseEntity<Optional<User>> getUserById(@PathVariable("id") String id){
-        System.out.println("test-merge2");
         return ResponseEntity.ofNullable(userService.findUserById(Integer.parseInt(id)));
     }
 }
